bc66093cd6a6581b860b641a7bf63eb1f682476c
root <root@CentOS7.x-StagDocker.1.78>
2022-06-28 20:53:04 +0700

    minimum very strong

--- test.py
+++ test.py
@@ -3,5 +3,4 @@ from rand_pass_generator import generate, check
 def testFunction():
     assert type(generate()) == str
 
-print(generate().numberLetter())
-print(check('54aB0e31ni'))
+print(generate().random())
